19f90f1ca810ca554d241329dbfbcdad14a0be24
Dawid Brzeski <daw.brzeski@gmail.com>
2021-08-10 15:06:22 +0200

    adding empty task blocked

--- src/Form/index.js
+++ src/Form/index.js
@@ -5,17 +5,22 @@ const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
     const onFormSubmit = (event) => {
         event.preventDefault();
-        addNewTask(newTaskContent.trim());
+        const trimmedNewTaskContent = newTaskContent.trim();
+
+        if (trimmedNewTaskContent === "") {
+            return;
+        }
+        addNewTask(trimmedNewTaskContent);
         setNewTaskContent("");
     };
 
     return (
         <form className="form" onSubmit={onFormSubmit}>
             <input value={newTaskContent}
-                autofocus
+                autoFocus
                 className="form__input"
                 placeholder="Co jest do zrobienia?"
-                onChange={({target}) => setNewTaskContent(target.value)}
+                onChange={({ target }) => setNewTaskContent(target.value)}
             ></input>
             <button className="form__button"> Dodaj zadanie</button>
         </form>
