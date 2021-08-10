f8c49b906d03aae359afcfe14b1abfefba35f241
Dawid Brzeski <daw.brzeski@gmail.com>
2021-08-10 14:26:11 +0200

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -30,7 +30,14 @@ function App() {
       }
       return task;
     }));
-  }
+  };
+
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task,
+      done: true,
+    })));
+  };
 
   return (
     <Container>
@@ -42,7 +49,7 @@ function App() {
       <Section
         title="Lista zadań"
         body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} toggleTaskDone={toggleTaskDone} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} />}
+        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} setAllDone={setAllDone} />}
       />
 
     </Container>
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone}) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
     <div className="buttons">
         {tasks.length > 0 && (
             <>
                 <button onClick={toggleHideDone} className="buttons_button">
                     {hideDone ? "Pokaz" : "Ukryj"} ukończone
                 </button>
-                <button className="buttons_button"
+                <button
+                    onClick={setAllDone}
+                    className="buttons_button"
                     disabled={tasks.every(({ done }) => done)}
                 >Ukończ wszsytkie
                 </button>
