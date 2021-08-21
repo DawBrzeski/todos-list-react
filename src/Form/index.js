798280468851dbabaf6a8a376c09993f515bfc83
Dawid Brzeski <daw.brzeski@gmail.com>
2021-08-21 13:31:04 +0200

    adding autofocus on input

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { StyledForm, Input, Button } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
+
     const onFormSubmit = (event) => {
         event.preventDefault();
         const trimmedNewTaskContent = newTaskContent.trim();
@@ -12,12 +14,14 @@ const Form = ({ addNewTask }) => {
         }
         addNewTask(trimmedNewTaskContent);
         setNewTaskContent("");
+        inputRef.current.focus();
     };
 
     return (
         <StyledForm
             onSubmit={onFormSubmit}>
             <Input
+                ref={inputRef}
                 value={newTaskContent}
                 placeholder="Co jest do zrobienia?"
                 onChange={({ target }) => setNewTaskContent(target.value)}
